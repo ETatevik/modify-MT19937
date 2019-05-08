adcdd3e8fd6b5bbe6c0b85290f21cc07730d1d86
Yegiazaryan Tatevik <barev.tatevik@gmail.com>
2019-05-08 03:06:41 +0300

    Start

---
+++ js/script.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function($) {
+	
+	$('.dateAath').html(`<a href="https://github.com/ETatevik" target="blank_">Yegizaryan Tatevik </a>&copy; ${new Date().getFullYear()} г.`);
+
+});
